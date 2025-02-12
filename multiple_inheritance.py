7591edbe5688b72d9f19d3edd4e405f5ee8ed9ef
rishita das <er.rishitadas@gmail.com>
2025-02-12 15:44:32 +0530

    Multiple-Inheritance

---
+++ multiple_inheritance.py
@@ -0,0 +1,17 @@
+class Father:
+    fathername=" "
+    def f(self):
+        print(self.fathername)
+class Mother:
+    mothername=" "
+    def m(self):
+        print(self.mothername)
+class Son(Father, Mother):
+    sonname=" "
+    def s(self):
+        print("Father is: ",self.fathername)
+        print("Mother is: ",self.mothername)
+obj=Son()
+obj.fathername="Ram"
+obj.mothername="Sita"
+obj.s()
