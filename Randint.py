6d7359ef598acc1366b6d0573b5326379ea60e01
rishita das <er.rishitadas@gmail.com>
2025-02-21 16:10:05 +0530

    Codes

---
+++ Randint.py
@@ -0,0 +1,9 @@
+#randint()
+#this function generates integers between a given limit.it takes 2 parameters where the first parameter
+#specifies the lower limit and second parameter specifies the upper limit.randint(a,b)starts generating values from a to b such that:
+#a<= x<= b (includes a, b)
+
+import random
+for x in range(2):
+    print(random.randint(2,9))
+    
